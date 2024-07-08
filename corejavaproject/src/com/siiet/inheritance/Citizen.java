55863c23e6f120211b75b2bd6bf8ab0cf0c6d05e
Neha reddy <Neha reddy@DESKTOP-VABLV4A>
2024-07-08 21:26:46 +0530

    next commit

--- corejavaproject/src/com/siiet/inheritance/Citizen.java
+++ corejavaproject/src/com/siiet/inheritance/Citizen.java
@@ -1,5 +1,67 @@
 package com.siiet.inheritance;
 
 public class Citizen {
+	private String name;
+	private long aadharno;
+	private String address;
+	private long phoneno;
+	
+	
+	public Citizen(String name,long aadharno,String address,long phoneno) {
+		super();
+		this.name=name;
+		this.aadharno=aadharno;
+		this.address=address;
+		this.phoneno=phoneno;
+		
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public long getAadharno() {
+		return aadharno;
+	}
+
+
+	public void setAadharno(long aadharno) {
+		this.aadharno = aadharno;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public long getPhoneno() {
+		return phoneno;
+	}
+
+
+	public void setPhoneno(long phoneno) {
+		this.phoneno = phoneno;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Citizen [name=" + name + ", aadharno=" + aadharno + ", address=" + address + ", phoneno=" + phoneno
+				+ "]";
+	}
+	
 
 }
